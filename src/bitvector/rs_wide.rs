de596fdae2f9e0b1470e1fc4f31ada1589bbb3f4
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-01-17 05:37:02 +0100

    Add RankBin::prefetch(pos) (#6)
    * Add RankBin::prefetch(pos)
    
    * fix copilot suggestions

--- src/bitvector/rs_wide.rs
+++ src/bitvector/rs_wide.rs
@@ -308,6 +308,16 @@ impl RankBin for RSWide {
         result
     }
 
+    #[inline(always)]
+    fn prefetch(&self, pos: usize) {
+        let pos = pos.wrapping_sub(1);
+        prefetch_read_NTA(&self.bv.data, pos >> 9);
+
+        let sub_block = pos >> 9;
+        let superblock = sub_block / (SUPERBLOCK_SIZE / BLOCK_SIZE);
+        prefetch_read_NTA(&self.superblock_metadata, superblock);
+    }
+
     fn n_zeros(&self) -> usize {
         self.n_zeros()
     }
