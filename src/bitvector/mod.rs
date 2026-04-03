19a6bc3eaa6a32121474283140e52917c6e18c8b
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2026-04-03 10:37:05 +0200

    Add bounds check to BitVectorMut::from_packed_data

--- src/bitvector/mod.rs
+++ src/bitvector/mod.rs
@@ -872,6 +872,10 @@ impl BitVectorMut {
 
     /// Construct a BitVector directly from already-packed data.
     ///
+    /// # Panics
+    ///
+    /// Panics if the length l of the data is 0 or n_bits is outside ((l-1)*64, l*64].
+    ///
     /// # Examples
     ///
     /// ```
@@ -884,6 +888,7 @@ impl BitVectorMut {
     #[must_use]
     #[inline]
     pub fn from_packed_data(data: &[u64], n_bits: usize) -> Self {
+        assert!(data.len() > 0 && n_bits > (data.len() - 1) * 64 && n_bits <= data.len() * 64);
         let mut v = BitVectorMut::with_capacity(n_bits);
         if let [rest @ .., last] = data {
             for d in rest {
